893b0ac167975c4ac21455094107ea383db53d03
Tj Thouhid <tjthouhid@gmail.com>
2018-02-28 16:17:38 +0600

    Initial Commit

---
+++ param-iframe-generator.php
@@ -0,0 +1,36 @@
+<?php
+	/**
+		* Plugin Name: Param Iframe Generator
+		* Plugin URI: http://www.frndzit.com/param-iframe-generator
+		* Description: This is plugin for creating iframe by current page get url parameter.
+		* Version: 1.0.1
+		* Author: Tj Thouhid
+		* Author URI: https://www.tjthouhid.com
+		* License: A "Slug" license name e.g. GPL12
+	**/
+	function param_iframe_generator( $atts ) {
+		$atts = shortcode_atts( array(
+			'url' => 'https://www.tjthouhid.com',
+			'iframe-width' => '400px',
+			'iframe-height' => '400px' 
+	
+		), $atts, 'param_iframe_generator' );
+			ob_start();
+			$str="";
+		    if(isset($_GET)){
+		    	foreach ($_GET as $key => $value) :
+		    		if($str==""){
+		    			$str.="?";
+		    		}else{
+		    			$str.="&";
+		    		}
+		    		$str.=$key."=".$value;
+		    	endforeach;
+		    }
+		    echo "<iframe src='".$atts['url'].$str."' width='".$atts['iframe-width']."' height='".$atts['iframe-height']."' ></iframe>";
+		    
+
+		     $content = ob_get_clean();
+		 	return $content;
+	}
+	add_shortcode( 'param-iframe-generator', 'param_iframe_generator' );
---
+++ readme.md
@@ -0,0 +1,32 @@
+# Param Iframe Generator
+
+Tags: iframe generator, wordpress iframe generator
+Requires at least: 3.0.1
+Tested up to: 4.9.4
+Requires PHP: 5.2.4
+Stable tag: 1.0.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+This is a plugin for Easy Digital Download Software License Activator Notification Show on Dashboard as widget
+ 
+== Description ==
+Version : 1.0.1
+This is plugin for creating iframe by current page get url parameter.
+WordPress.Org link : https://wordpress.org/plugins/param-iframe-generator/
+Github link : https://github.com/tjthouhid/Param-Iframe-Generator
+
+== Installation ==
+1. Download param-iframe-generator.zip and extract it.
+2. Upload param-iframe-generator folder to the /wp-content/plugins/ directory  
+3. Activate the plugin through the 'Plugins' menu in WordPress
+
+Now Just Use it as shortcode [param-iframe-generator url="https://www.example.com" iframe-width="400px" iframe-height="400px"]
+
+== Screenshots ==
+ 
+1. screenshot-1.png
+2. screenshot-2.png
+
+
+== Changelog ==
